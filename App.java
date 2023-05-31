40074a6e3cbf27008328a378eee116adb0def9a1
MauricioBV <124370784+MauricioBV@users.noreply.github.com>
2023-05-31 08:10:25 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,29 @@
+package com.pp2;
+
+public class App {
+
+	
+		public static void drawLegs(){
+	        System.out.println("     ||     ||      ");
+	        System.out.println("     ||     ||      ");
+	        System.out.println("    (||)   (||)     ");
+	        System.out.println("    UUUU   UUUU     ");
+	    }
+	    
+	    public static void main(String[] args) {
+	    	System.out.println("*****PROGRAMICHI*****");
+	        System.out.println("   /\\         /\\   ");
+	        System.out.println("  /  \\_______/  \\  ");
+	        System.out.println(" /               \\ ");
+	        System.out.println("(  /\\         /\\  )");
+	        System.out.println("~~~~     V     ~~~~");
+	        System.out.println("~~~~~~(__|__)~~~~~~");
+	        System.out.println("  (             )  ");
+	        System.out.println("   (___________)   ");
+	        drawLegs();
+	        
+	    }    
+
+	}
+
+
